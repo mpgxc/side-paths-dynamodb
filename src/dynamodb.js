7d636aadc5c370ac6c611516bf26a55f061710bf
mpgxc <mpgx5.c@gmail.com>
2022-08-10 23:54:36 -0300

    feat: added first method

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,23 +1,76 @@
-const AWS = require('aws-sdk')
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb')
 
-AWS.config.loadFromPath('./config.json')
+const client = new DynamoDBClient({
+  region: 'us-east-1',
+})
 
-const documentClient = new AWS.DynamoDB.DocumentClient()
+const TableName = 'application-table'
+const buildPartitionKey = (param) => ({
+  S: `USER-${param}`,
+})
 
-const createUser = () => {
+const buildSortKey = (param) => ({
+  S: `PROFILE-${param}`,
+})
+
+const parseParams = (param) =>
+  ({
+    string: { S: param },
+
+    /**
+     * Resolvendo problema da conversão de números
+     * https://stackoverflow.com/questions/71488712/number-value-cannot-be-converted-to-string-when-updating-item
+     * */
+
+    number: { N: `${param}` },
+    boolean: { BOOL: param },
+    object: { M: param },
+    array: { L: param },
+    null: { NULL: param },
+  }[typeof param])
+
+const buildParams = (params) =>
+  Object.assign(
+    {},
+    ...Object.keys(params).map((index) => ({
+      [index]: parseParams(params[index]),
+    }))
+  )
+
+const createUser = async (params) => {
+  const Item = {
+    PK: buildPartitionKey(params.userName),
+    SK: buildSortKey(params.userName),
+    ...buildParams(params),
+  }
+
+  try {
+    return client.send(
+      new PutItemCommand({
+        Item,
+        TableName,
+      })
+    )
+  } catch (err) {
+    console.error({ err })
+  }
 }
 
-const createTicket = () => {
-}
+createUser({
+  address: 'Rua. Paulo Bernardino',
+  email: 'garcia@email.com',
+  fullName: 'Mateus Garcia',
+  userName: 'mpgxc',
+  age: 26,
+}).then()
 
-const getUser = () => {
-}
+const createTicket = () => {}
 
-const getUserTickets = () => {
-}
+const getUser = () => {}
 
-const getUserAndTickets = () => {
-}
+const getUserTickets = () => {}
+
+const getUserAndTickets = () => {}
 
 module.exports = {
   createUser,
